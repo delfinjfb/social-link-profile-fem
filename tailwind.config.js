c4f1c1f636fa9ec289e93f35183594a4afd7e94e
Delfin F <DelfinoDevShark@gmail.com>
2024-07-24 15:35:49 +0200

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./app/**/*.{js,ts,jsx,tsx}"], // Point to your Remix app files
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
