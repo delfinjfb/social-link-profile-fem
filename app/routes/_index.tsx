ca34cbe7be405454e95aa17cfe33277af6c3dc00
Delfin F <DelfinoDevShark@gmail.com>
2024-07-24 20:02:26 +0200

    fix Meta tags

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,5 @@
-// app/routes/_index.tsx
-import type {MetaFunction} from "@remix-run/node";
 import {redirect} from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-	return [
-		{title: "Social Networks - Delfin Fernandez"},
-		{name: "description", content: "Welcome to Remix!"}
-	];
-};
-
 export const loader = () => redirect("/profile");
+
+// You can remove the `meta` export as it's not needed here.
